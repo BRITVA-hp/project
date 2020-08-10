8324ee1adb11bf4143a71ba28c12b3ea78d72af7
Eduard <BRITVA_hp1989@mail.ru>
2020-08-10 18:43:25 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert(`Привет, ${user}`);
